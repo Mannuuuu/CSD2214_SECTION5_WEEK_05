3695ca6677539fdd3219da935084dd6c17c33400
Manpreet Kaur <mani.patwaal24@gmail.com>
2019-02-07 11:43:37 -0500

    i commit

---
+++ week005/week005/js/main.js
@@ -0,0 +1,9 @@
+﻿function myFunction(a, b){
+    return a+b;  // The function returns he product of p1 and p2
+}
+
+function arrays() {
+    var pens;
+    pens = ["red", "orange", "green", "blue", "purple"];
+    console.log(pens);
+}
